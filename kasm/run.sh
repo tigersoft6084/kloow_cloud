bfc61e301248550cc2e718562c41d29b3f737fba
NinjaCoder0516 <haotian.song.0516@gmail.com>
2025-08-05 15:34:11 +0000

    Remove server logic Remove kasm logic

--- kasm/run.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-  --name $1 \
-  -p $4:6901 \
-  --shm-size=2g \
-  -e VNC_PW=password \
-  -e VNCOPTIONS="-disableBasicAuth" \
-  -e LAUNCH_URL=$2 \
-  -e PROXY_URL=$3 \
-  proxylogin-chrome:latest
--- kasm/stop.sh
+++
@@ -1,4 +0,0 @@
-docker stop $1
-docker rm $1
-
-#docker rm -f $(docker ps -a -q)
